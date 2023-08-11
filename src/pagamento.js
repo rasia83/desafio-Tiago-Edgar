35d990caccb430a043f189ecfe4976a54461aefd
Tiago Edgar Rasia <103585675+rasia83@users.noreply.github.com>
2023-08-11 15:52:48 -0300

    formas de pagamento

---
+++ src/pagamento.js
@@ -0,0 +1,5 @@
+export const tabelaPagamento = {
+  dinheiro: { descricao: 'Dinheiro', modificador: 0.95 },
+  debito: { descricao: 'Débito', modificador: 1.0 },
+  credito: { descricao: 'Crédito', modificador: 1.03 }
+};
