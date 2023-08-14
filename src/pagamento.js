50f4861dead1af657774062f4d08dd175a0a5917
Tiago Edgar Rasia <103585675+rasia83@users.noreply.github.com>
2023-08-14 12:28:46 -0300

    indentação

--- src/pagamento.js
+++ src/pagamento.js
@@ -1,5 +1,5 @@
 export const tabelaPagamento = {
-  dinheiro: { descricao: 'Dinheiro', modificador: 0.95 },
-  debito: { descricao: 'Débito', modificador: 1.0 },
-  credito: { descricao: 'Crédito', modificador: 1.03 }
+    dinheiro: { descricao: "Dinheiro", modificador: 0.95 },
+    debito: { descricao: "Débito", modificador: 1.0 },
+    credito: { descricao: "Crédito", modificador: 1.03 },
 };
