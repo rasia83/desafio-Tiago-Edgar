fe1ecf090c86739f75b70079a4e3daa60283d4f7
Tiago Edgar Rasia <103585675+rasia83@users.noreply.github.com>
2023-08-14 08:55:31 -0300

    modificando os combos e indentando código

--- src/cardapio.js
+++ src/cardapio.js
@@ -1,10 +1,10 @@
 export const tabelaValores = {
-  cafe: { descricao: 'Café', valor: 3.0, dependencia: null },
-  chantily: { descricao: 'Chantily (extra do Café)', valor: 1.5, dependencia: 'cafe' },
-  suco: { descricao: 'Suco Natural', valor: 6.2, dependencia: null },
-  sanduiche: { descricao: 'Sanduíche', valor: 6.5, dependencia: null },
-  queijo: { descricao: 'Queijo (extra do Sanduíche)', valor: 2.0, dependencia: 'sanduiche' },
-  salgado: { descricao: 'Salgado', valor: 7.25, dependencia: null },
-  combo1: { descricao: '1 Suco e 1 Sanduíche', valor: 9.50, dependencia: ['suco', 'sanduiche'] },
-  combo2: { descricao: '1 Café e 1 Sanduíche', valor: 7.5, dependencia: ['cafe', 'sanduiche'] }
+    cafe: { descricao: 'Café', valor: 3.0, dependencia: null },
+    chantily: { descricao: 'Chantily (extra do Café)', valor: 1.5, dependencia: 'cafe' },
+    suco: { descricao: 'Suco Natural', valor: 6.2, dependencia: null },
+    sanduiche: { descricao: 'Sanduíche', valor: 6.5, dependencia: null },
+    queijo: { descricao: 'Queijo (extra do Sanduíche)', valor: 2.0, dependencia: 'sanduiche' },
+    salgado: { descricao: 'Salgado', valor: 7.25, dependencia: null },
+    combo1: { descricao: '1 Suco e 1 Sanduíche', valor: 9.50, dependencia: null}, 
+    combo2: { descricao: '1 Café e 1 Sanduíche', valor: 7.5, dependencia: null }  
 };
